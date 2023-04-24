530103f69a65e82a48e689c871b95099dd770439
bctboi23 <35770571+bctboi23@users.noreply.github.com>
2023-04-24 00:10:38 -0500

    Update anisotropic_diffusion_denoising.py

--- CSCI5561_Refactored/anisotropic_diffusion_denoising.py
+++ CSCI5561_Refactored/anisotropic_diffusion_denoising.py
@@ -1,6 +1,9 @@
 import numpy as np
+import cv2
+from skimage.restoration import denoise_tv_chambolle as tv_denoise
+from skimage.restoration import denoise_wavelet as wl_denoise
 
-def anisodiff(img, niter=1, kappa=50, gamma=0.1, sigma=0):
+def anisodiff(img, niter=1, kappa=50, gamma=0.1):
     
     # Anisotropic diffusion denoising
     # Based on the python2 implementation by Alistair Muldal <alistair.muldal@pharm.ox.ac.uk>
@@ -16,10 +19,6 @@ def anisodiff(img, niter=1, kappa=50, gamma=0.1, sigma=0):
     NS = np.zeros(imgout.shape)
     EW = np.zeros(imgout.shape)
 
-    # initialize gradients
-    gS = np.ones(imgout.shape)
-    gE = np.ones(imgout.shape)
-
     for ii in np.arange(1,niter):
 
         # calculate the diffs
@@ -32,9 +31,9 @@ def anisodiff(img, niter=1, kappa=50, gamma=0.1, sigma=0):
         E = np.exp(-(deltaEf/kappa)**2.)*deltaE
         S = np.exp(-(deltaSf/kappa)**2.)*deltaS
         
-        # subtract a copy that has been shifted 'North/West' by 1 pixel
-        # (this needs to be here??? Breaks without it)
-        # "don't [ask] questions. just do it. trust me." - Alistair
+        # Subtract a copy that has been shifted 'North/West' by 1 pixel
+        # (This needs to be here??? Breaks without it, not sure why)
+        # "Don't [ask] questions. just do it. trust me." - Alistair
         NS[:] = S
         EW[:] = E
         NS[1:,:] -= S[:-1,:]
@@ -52,11 +51,13 @@ def filter_image(im, filter):
     return np.einsum('ij,ijkl->kl', filter, subM)
 
 def denoise_img(img):
+    # expects an image of (H, W, C) with values from 0 to 1
     img_out = np.zeros(img.shape)
+    
     for x in range(img.shape[2]):
-        img_out[:,:,x] = anisodiff(img[:,:,x],niter=50,kappa=80,gamma=0.025)
-        # sharpen output
+        img_out[:,:,x] = anisodiff(img[:,:,x], 50, 80, 0.02)
+        # sharpen per channel output with modified laplacian filter
         kernel = np.array([[-0.5,-1,-0.5], [-1,7,-1], [-0.5,-1,-0.5]])
         img_out[:,:,x] = filter_image(img_out[:,:,x], kernel)
-    
-    return img_out
+
+    return np.clip(img_out, 0., 1.)
