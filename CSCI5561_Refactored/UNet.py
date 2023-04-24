a53c05e4a51d97540c795933911da0e46da71cd1
Bin <82672951+binhu02@users.noreply.github.com>
2023-04-23 23:05:51 -0500

    Add report, model save

--- CSCI5561_Refactored/UNet.py
+++ CSCI5561_Refactored/UNet.py
@@ -154,3 +154,35 @@ class UNet3(nn.Module):
         self.up2 = torch.utils.checkpoint(self.up2)
         self.up3 = torch.utils.checkpoint(self.up3)
         self.outc = torch.utils.checkpoint(self.outc)
+
+
+class UNet2(nn.Module):
+    def __init__(self, n_channels=4, n_classes=5, bilinear=False):
+        super(UNet2, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256 // 2)
+        self.up1 = Up(256, 128 // 2, bilinear)
+        self.up2 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x = self.up1(x3, x2)
+        x = self.up2(x, x1)
+        logits = self.outc(x)
+        return logits
+        
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.outc = torch.utils.checkpoint(self.outc)
